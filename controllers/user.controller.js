800f91fb59427b92b12e0f7649579849372a05fc
angpao <angpao.kittimasak@gmail.com>
2019-04-13 22:04:08 +0700

    Create function getUserProfile

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const passport = require('passport');
 const User = require('../models/user.model')
+const _ = require('lodash');
 
 module.exports.register = (req, res, next) => {
     const user = new User(req.body)
@@ -27,4 +28,15 @@ module.exports.authenticate = (req, res, next) => {
         // unknown user or wrong password
         else return res.status(404).json(info);
     })(req, res);
+}
+
+module.exports.userProfile = (req, res, next) => {
+    User.findOne({ _id: req._id },
+        (err, user) => {
+            if (!user)
+                return res.status(404).json({ status: false, message: 'User record not found.' });
+            else
+                return res.status(200).json({ status: true, user: _.pick(user, ['_id', 'userName', 'email']) });
+        }
+    );
 }
