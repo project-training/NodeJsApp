12d9f5714455264a5153e8d9bce322ced8c2e8f1
angpao <angpao.kittimasak@gmail.com>
2019-04-13 13:56:34 +0700

    Add validate email

--- models/user.model.js
+++ models/user.model.js
@@ -11,6 +11,13 @@ let UserSchema = new Schema({
     saltSecret: String
 })
 
+// Custom validation for email
+userSchema.path('email').validate((val) => {
+    emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(val);
+}, 'Invalid e-mail.');
+
+// Encrypt for password
 UserSchema.pre('save', function (next) {
     bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(this.password, salt, (err, hash) => {
