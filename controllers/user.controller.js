f0e7d014540da5e83b6c3b524ea4e47f93e3f298
angpao <angpao.kittimasak@gmail.com>
2019-04-13 13:38:13 +0700

    Function Register already

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,11 @@
+const mongoose = require('mongoose')
+const User = require('../models/user.model')
+
 module.exports.register = (req, res, next) => {
-    console.log('inside register fn. ')
+    const user = new User(req.body)
+    user.save((err, doc) => {
+        if (!err) {
+            res.status(201).send(doc)
+        }
+    })
 }
